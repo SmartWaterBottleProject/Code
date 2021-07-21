4ca8f5cb26a27bc2b012e9ea8b7b0436d722f498
Ryan <SmartWaterBottleProject@protonmail.com>
2021-07-20 21:20:33 -0400

    Working PWMServo code, PWM signal moved to P1.6

--- SmartWaterBottleProject/PWM Servomotor Demo/main.c
+++ SmartWaterBottleProject/PWM Servomotor Demo/main.c
@@ -15,12 +15,29 @@
 //#include "driverlib.h"
 #define Button1 BIT1     //P1.1
 #define Button2 BIT2     //P1.2
+#define PWMServo BIT6    //P1.6
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;       //clear lock and allow new code re-flash/enable GPIO Pins
 
+
+
+    //Configure P1.6 as TA0.1 for PWM
+    P1DIR |= PWMServo;
+    P1SEL1 |= PWMServo;
+    P1SEL0 |= PWMServo;
+
+    TA0CCR0=20000-1; //@1 Mhz, 20 millisecond
+    TA0CTL= TASSEL_2 | ID_0 | MC_1 | TACLR;
+    //Channel Reset/Set
+
+    TA0CCTL1 |= OUTMOD_7;
+    TA0CCR1=1500-1;
+
+
+    /*
     // Configure P1.4 as TA1.0 for PWM
        //P1DIR.4=1, PSEL1.4=1, P1SEL0.4=1, LCDSz=0
        P1DIR |= BIT4; //SetP1DIR bit 4 as high
@@ -33,10 +50,11 @@ int main(void)
     //TACCR1 count for total time
     TA1CCR1 = 20000-1; //@1Mhz, T=20 ms, (f=50 Hz)
 
+
     //Configure clock with SMCLK=1Mhz, No input division, Up mode, and clear TAR
     TA1CTL = TASSEL_2 | ID_0 | MC_1 | TACLR;
-    TA1CCTL0 |= OUTMOD_3; // Set/Reset Output Pattern
-    TA1CCR0=1500-1; //@1MHz, T= 1.5 ms (90 degree angle)
+    TA1CCTL0 |= OUTMOD_7; // Set/Reset Output Pattern
+    TA1CCR0=1500-1; //@1MHz, T= 1.5 ms (90 degree angle)*/
 
 
 
@@ -62,29 +80,21 @@ while(1){}
 
 //**********ISRs********//
 //*********************//
-/*
-// ISR of Timer 1 Channel 0 (A0 vector)
-#pragma vector = TIMER1_A0_VECTOR
-__interrupt void T1A0_ISR() {
- // Toggle the green LED
-    //P9OUT ^= greenLED;
-}*/
-
 
 #pragma vector = PORT1_VECTOR  //Link the ISR to the Vector
 __interrupt void P1_ISR()
 {
    if( (P1IFG&Button1) !=0 ) {
        //decrement duty cycle by 10 microseconds
-       if(TA1CCR0 > (1500-1)  )
-           TA1CCR0-=10;
+       if(TA0CCR1 > (1500-1)  )
+           TA0CCR1-=10;
    P1IFG &= ~Button1; //clear flag
    }
 
    if( (P1IFG&Button2)!= 0 ) {
        //increment duty cycle by 10 microseconds
-       if(TA1CCR0 < (1560-1)  )
-           TA1CCR0+=10;
+       if(TA0CCR1 < (1560-1)  )
+           TA0CCR1+=10;
    P1IFG &= ~Button2; //clear flag
    }
 

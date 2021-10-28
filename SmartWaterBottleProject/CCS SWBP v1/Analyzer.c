d959b48ddb49e883d725653ebd99de6e12928b6d
Ryan <SmartWaterBottleProject@protonmail.com>
2021-10-28 18:28:33 -0400

    Fixed variable names, which prevented previous version from building correctly. -RKK

--- SmartWaterBottleProject/CCS SWBP v1/Analyzer.c
+++ SmartWaterBottleProject/CCS SWBP v1/Analyzer.c
@@ -11,11 +11,7 @@
 #include <stdlib.h>
 
 //Min pulse width for stepper is 1us
-
-//Something might be wrong with continue and break in configuration loop
-
 //Treat voltage as adc integer or convert back to voltage
-
 //Could eventually take multiple channel measurements of photodiode voltage
 
 #define ZERO 0   //Value below expected peak ADC reading in LPM was previously 65535
@@ -33,7 +29,6 @@ uint16_t * Analyze(void)
 
     uint16_t MAX = ZERO;      //Measured Max. value from ADC read
     uint16_t Voltage;         //Stores voltage from ADC
-    uint16_t BASELINE=0;
     uint16_t i=0;  //For loop counter that runs WIDTH times during analysis
     uint16_t j=0;  //For loop counter
     bool PassedPeak = 0;      //Have we passed the peak, 0-no, 1-yes
@@ -92,7 +87,7 @@ uint16_t * Analyze(void)
 
                //Dynamic memory allocation for SAMPLES, here
               uint16_t * SAMPLES;
-              SAMPLES = (uint16_t *)calloc(POSITION, sizeof(uint16_t)); //Dynamically allocate memory for samples array, size of position
+              SAMPLES = (uint16_t *)calloc(Position, sizeof(uint16_t)); //Dynamically allocate memory for samples array, size of position
               Step(WIDTH*2);  //Step down by Width*2 Units
 
               //Initialize Baseline variables
@@ -139,7 +134,7 @@ uint16_t * Analyze(void)
                   }
               }
 
-                  for(i=WIDTH; i<= POSITION; i++)  //For loop that iterates from WIDTH to POSITION using i
+                  for(i=WIDTH; i<= Position; i++)  //For loop that iterates from WIDTH to POSITION using i
                   {
                       Voltage = GetVoltage();
                       SAMPLES[i] = Voltage;
@@ -193,78 +188,7 @@ uint16_t GetVoltage()
 	return PhotodiodeVoltage;
 }
 
-
-
-
-
-
-
-
-////Analyzer Program Pseudocode
-//
-////Define a constant named ZERO to represent a value just below the expected zero'd value (determined experimentally)
-////Define a constant named POSTZERO.  This represents how many positions from zero the stepper should move after the zeroing run (determined experimentally)
-////Define a constant named BASELINE.  This represents what can be considered a zero-voltage (determined experimentally).
-////Define a constant named WIDTH.  This represents the number of steps needed to move the photodiode entirely past a sample (determined experimentally).
-//	//To clarify, this is the width of the photodiode's element divided by the step length of the stepper motor.
-////Define ONE as 1.  This represents a single step.
-//	//This may look dumb, but it lets us switch the directly we want the program to iterate later on with fewer alterations.
-//
-//
-////****************************************Performed in Main*********************************/
-////Button pressed
-//	//Pre-run check
-//		//Check the reed switch to determine if the cap is closed
-//		//Check the battery level to determine if there is probably enough energy for a run
-////****************************************Performed in Main*********************************/
-//
-//	//Analyzer Operation
-//		//If stepper
-//			//Zeroing-Run
-//				//Turn on laser to the low power setting
-//				//Initialize MAX = ZERO, PEAK = 0;
-//				//Get VOLT
-//				//If the photodiode's voltage (VOLT) is more than half of MAX, step down by POSTZERO.
-//				//Infinite While loop
-//					//Step the stepper motor up
-//					//Get VOLT
-//					//Compare the photodiode's voltage to the MAX variable.
-//						//If (MAX < VOLT) then MAX = VOLT and PEAK = 1;
-//						//If (MAX > VOLT && PEAK == 1) exit the loop.  This means that we have passed the maximum point (the zero).
-//				//int POSITION = Step(POSTZERO).  This moves the stepper POSTZERO positions and records how far it is from the zero.
-//			//Analysis run
-//				//Infinite While Loop
-//					//Infinite While Loop
-//						//Get VOLT
-//						//If VOLT is greater than BASELINE
-//							//POSITION += Step(-ONE);
-//						//If the VOLT is equal to or below BASELINE, break.
-//					//Allocate memory for an integer array that is POSITION units long named Samples.
-//					//Step down by 2*WIDTH units.
-//					//Initialize unsigned integer variables BaselineMax = 0, BaselineMin = -1 (overflows to the max value), Baseline.
-//					//Baseline = VOLT/WIDTH (gets the average entry weight)
-//					//For loop that runs WIDTH times
-//						//Position += Step(ONE);
-//						//Get VOLT
-//						//If VOLT > BaselineMax, BaselineMax = VOLT
-//						//If VOLT < BaselineMin, BaselineMin = VOLT
-//					//If BaselineMax - BaselineMix > Baseline 	--This means that there is light incident at some point.
-//						//Free Samples (if using allocated memory)
-//						//Step down by 2*WIDTH times
-//						//Continue;		--Jumps back to the beginning and starts the anaylsis over from the new position.
-//					//Break;		--Exits the configuration while-loop and starts actually analyzing.
-//				//For loop that iterates from 0 to WIDTH using i.
-//					//Get VOLT
-//					//Samples[i] = VOLT - (WIDTH-1-i)*Baseline;
-//					//for(j=i-1;j>=0;j--)
-//						//Samples[i] = Samples[i] - Samples[j];
-//				//For Loop that iterates from WIDTH to POSITION using i.
-//					//Get VOLT
-//					//Samples[i] = VOLT;
-//					//for(j=i-1;j>i-WIDTH+1;j--)
-//						//Samples[i] = Samples[i] - Samples[j];
-//				//--Now we have a vector of samples
-
-
 //Once finished call exporter to send data via bluetooth
+
+
 //Servo may be utilized later
